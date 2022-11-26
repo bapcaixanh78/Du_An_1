bb79a50281240a98e7640c54825c0815e1f6194c
Ajanokouji <trihnph26922@fpt.edu.vn>
2022-11-26 15:26:21 +0700

    Tri2611

---
+++ 1.DAL/Models/PointsFund.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DAL.Models
+{
+    public class PointsFund
+    {
+        [Key]
+        public Guid IdPFund { get; set; }
+
+        public Decimal PointsMoney { get; set; }
+
+        public Decimal PointsUsed { get; set; }
+
+        public int Status { get; set; }
+
+        public virtual HistoryUsingPoints HPoints { get; set; }
+    }
+}
