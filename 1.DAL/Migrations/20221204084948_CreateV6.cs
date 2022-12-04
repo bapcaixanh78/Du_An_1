46d8f2b04157a6864f447f11c31b1bca8731c22b
Ajanokouji <trihnph26922@fpt.edu.vn>
2022-12-04 16:14:02 +0700

    Cập nhật Db

---
+++ 1.DAL/Migrations/20221204084948_CreateV6.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _1.DAL.Migrations
+{
+    public partial class CreateV6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_Username",
+                table: "Account",
+                column: "Username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Account");
+        }
+    }
+}
